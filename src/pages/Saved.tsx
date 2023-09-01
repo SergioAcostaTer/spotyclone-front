4e5f85aa10c825c4383e98ef3cdcaaafd245c1e5
Sergio Acosta <sergioacostaquintana@gmail.com>
2023-09-01 13:50:31 +0100

    shuffle

--- src/pages/Saved.tsx
+++ src/pages/Saved.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import usePlayer from '../hooks/usePlayer';
-import SongCard from '../components/SongCard';
-
+import React from "react";
+import usePlayer from "../hooks/usePlayer";
+import SongCard from "../components/SongCard";
 
 const Saved: React.FC = () => {
-  const { likedSongs, nextSong, prevSong } = usePlayer();
-  const [playLiked] = usePlayer(state => [state.playLiked])
+    const [playLiked, shuffle, likedSongs, nextSong, prevSong] = usePlayer(
+    (state) => [
+      state.playLiked,
+      state.shuffle,
+      state.likedSongs,
+      state.nextSong,
+      state.prevSong,
+    ]
+  );
 
   return (
-    <div className='w-full'>
+    <div className="w-full">
       <h1>Liked Songs</h1>
       <h2 onClick={prevSong}>prev</h2>
 
       <h2 onClick={playLiked}>Play all</h2>
       <h2 onClick={nextSong}>next</h2>
+      <h2 onClick={shuffle}>shuffle</h2>
+
       {likedSongs.length === 0 ? (
         <p>No liked songs available.</p>
       ) : (
-        <div className='w-full'>
+        <div className="w-full">
           {[...likedSongs].map((song) => (
             <SongCard key={song.id} song={song} />
           ))}
